73a2bf731caa38c0125a228505b060abaadb9aef
varshakmurthy <142469321+varshakmurthy@users.noreply.github.com>
2023-08-18 04:47:09 +0100

    Update CommonPage.java

--- src/test/java/pages/CommonPage.java
+++ src/test/java/pages/CommonPage.java
@@ -10,5 +10,10 @@ public class CommonPage extends Driver {
     public static void getUrl(final String url) {
         driver.navigate().to(url);
     }
+    public static void selectLink(String linkText) {
+    	linkText= "Find company information";
+    	WebElement linkElement = driver.findElement(By.linkText(linkText));
+    	linkElement.click();
+    	}
 
 }
