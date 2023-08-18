c314b865a9d9e09deacb5b9293540b306031e691
varshakmurthy <142469321+varshakmurthy@users.noreply.github.com>
2023-08-18 04:48:25 +0100

    Update CommonStepdefs.java

--- src/test/java/steps/CommonStepdefs.java
+++ src/test/java/steps/CommonStepdefs.java
@@ -15,6 +15,30 @@ public class CommonStepdefs extends CommonPage {
     public static void iGoToUrl(String url) {
         getUrl(url);
     }
+     @Given("I select the {string} link")
+    public void i_select_the_link(String link) {
+		// Write code here that turns the phrase above into concrete actions
+    	selectLink("selectLink");
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should be on the {string} page")
+    public void i_should_be_on_the_page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I click the Start now button")
+    public void i_click_the_start_now_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I search for company {string} and view the company details")
+    public void i_search_for_company_and_view_the_company_details(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
 
     @Then("I validate that the details are displayed")
